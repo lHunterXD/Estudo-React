856f0bd2c6e8c20047f828bc5eac734e0aac0653
Douglas <douglas.l2.hunter@gmail.com>
2024-04-22 22:10:37 -0300

    2-Aula

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 
 import './App.css'
-
+import HelloWord from './components/helloWord'
 function App() {
+
+  const name = "Douglas"
+
+  const newName = name.toUpperCase()
+
+  function sum(a, b){
+    return a + b
+  }
+
+  const url = 'https://via.placeholder.com/150'
 
   return (
     <>
     <div className='App'>
-      <h1>Ola React!</h1>
-
+      <HelloWord/>
+      <h1>Alterando o JSX</h1>
+      <p>Ola, {newName}</p>
+      <p>Soma: {sum(1, 2)}</p>
+      <img src={url} alt="Minha imagem" />
     </div>
     
     </>
---
+++ src/components/helloWord.jsx
@@ -0,0 +1,13 @@
+import Frase from "./frase"
+
+function HelloWord (){
+    return (
+    <div>
+        <h1>Meu Primeiro Componente</h1>
+        <Frase/>
+    </div>
+    )
+
+}
+
+export default HelloWord
